ba03ba070807b7c78e89609982e28229b3901335
nithinanand04 <anand118@purdue.edu>
2024-09-22 01:14:09 -0400

    updates

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    testMatch: [
+  testMatch: [
       "**/__tests__/**/*.js?(x)",
       "**/?(*.)+(spec|test).js?(x)"
-    ]
+    ],
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  }
 };
